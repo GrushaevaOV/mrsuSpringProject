b151c25d6cae8126b61320f66fd5030b5f950c36
Malko <MalkovMVE@yandex.ru>
2023-02-20 19:40:21 +0300

    init

---
+++ src/main/java/com/mrsu/IDEMain.java
@@ -0,0 +1,13 @@
+package com.mrsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDEMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDEMain.class, args);
+    }
+
+}
